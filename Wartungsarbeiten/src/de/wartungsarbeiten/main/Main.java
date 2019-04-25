5bc57d7113e51b66fd0cffdf6bea87da7bd00e30
tempoo5 <49492894+tempoo5@users.noreply.github.com>
2019-04-25 18:06:48 +0200

    Add files via upload

--- Wartungsarbeiten/src/de/wartungsarbeiten/main/Main.java
+++ Wartungsarbeiten/src/de/wartungsarbeiten/main/Main.java
@@ -1,37 +1,49 @@
 package de.wartungsarbeiten.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginManager;
+
 
 public class Main extends JavaPlugin implements Listener {
 
 	private boolean _wartung = false;
-	
+
 	public void onEnable(){
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		
 		getCommand("wartung").setExecutor(this);
+		
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		 
+		
 	}
-	
+
 	@EventHandler
 	public void LogIn(PlayerLoginEvent e) {
+		if(e.getPlayer().isOp() == false) {
+			e.disallow(null, "§4WARTUNGSARBEITEN \n §cmomentan wird der Server gewartet");
 		if(e.getPlayer().isOp() == false && _wartung) {
-			e.disallow(null, "ï¿½4WARTUNGSARBEITEN \n ï¿½cmomentan wird der Server gewartet");
+			e.disallow(null, "§4WARTUNGSARBEITEN \n §cmomentan wird der Server gewartet");
+		}
 		}
 	}
+
 
 	@EventHandler
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(sender instanceof Player) {
 			Player player = (Player) sender;
 			if(player.isOp())
-			{
-				 _wartung = !_wartung;
-				 player.sendMessage("Wartungsstatus: " + _wartung);
-			}
+				if(player.isOp())
+				{
+					 _wartung = !_wartung;
+					 player.sendMessage("Wartungsstatus: " + _wartung);
+				}
 		}
 		return false;
 	}
