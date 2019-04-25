8f308f1751388cdddc246a5c318ae236862e626c
AdrianKurth <adriankurth.privat@gmail.com>
2019-04-25 17:31:40 +0200

    Testerweiterung

--- Wartungsarbeiten/src/de/wartungsarbeiten/main/Main.java
+++ Wartungsarbeiten/src/de/wartungsarbeiten/main/Main.java
@@ -5,20 +5,31 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
 public class Main extends JavaPlugin implements Listener {
+
+	private boolean _wartung = false;
 	
 	public void onEnable(){
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		 
+		getCommand("wartung").setExecutor(this);
 	}
 	
 	@EventHandler
 	public void LogIn(PlayerLoginEvent e) {
-		if(e.getPlayer().isOp() == false) {
-			e.disallow(null, "§4WARTUNGSARBEITEN \n §cmomentan wird der Server gewartet");
+		if(e.getPlayer().isOp() == false && _wartung) {
+			e.disallow(null, "ï¿½4WARTUNGSARBEITEN \n ï¿½cmomentan wird der Server gewartet");
 		}
 	}
-	
+
+	@EventHandler
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player player = (Player) sender;
+			if(player.isOp()) _wartung = true;
+		}
+		return false;
+	}
 
 }
